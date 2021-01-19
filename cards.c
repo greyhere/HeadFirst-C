5e4c22a15eddd910e897d0a924bdf218c4fff28c
greyhere <57859708+greyhere@users.noreply.github.com>
2021-01-19 12:24:31 +0000

    add card counting

--- cards.c
+++ cards.c
@@ -21,6 +21,12 @@ int main()
         val = atoi(card_name);
     }
     
-    printf("The card value is: %i\n", val);
+    /* Check if the value is 3 to 6 */
+    if ((val > 2) && (val < 7)) {
+        puts("Card has gone up");
+    /* Otherwise check if the card was 10, J, Q, or K */
+    } else if (val == 10) {
+        puts("Card has gone down");
+    }
     return 0;
 }
