6b1bce8620d5c0d9cffa9b1bf0cce65919adad60
greyhere <57859708+greyhere@users.noreply.github.com>
2021-01-19 17:53:43 +0000

    using while loop

--- cards.c
+++ cards.c
@@ -5,29 +5,39 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card name: ");
-    scanf("%2s", card_name);
+    int count = 0;
+    while (card_name[0] != 'X') {
+        puts("Enter the card name: ");
+        scanf("%2s", card_name);
     
-    int val = 0;
-    switch (card_name[0]) {
-        case 'K':
-        case 'Q':
-        case 'J':
-            val = 10;
-            break;
-        case 'A':
-            val = 11;
-            break;
-        default:
-            val = atoi(card_name);
-    }
-    
-    /* Check if the value is 3 to 6 */
-    if ((val > 2) && (val < 7)) {
-        puts("Card has gone up");
-    /* Otherwise check if the card was 10, J, Q, or K */
-    } else if (val == 10) {
-        puts("Card has gone down");
+        int val = 0;
+        switch (card_name[0]) {
+            case 'K':
+            case 'Q':
+            case 'J':
+                val = 10;
+                break;
+            case 'A':
+                val = 11;
+                break;
+            case 'X':
+                continue;
+            default:
+                val = atoi(card_name);
+                if ((val < 1) || (val >10)) {
+                    puts("I don't understand that value!");
+                    continue;
+                }
+        }
+        
+        /* Check if the value is 3 to 6 */
+        if ((val > 2) && (val < 7)) {
+            count++;
+        /* Otherwise check if the card was 10, J, Q, or K */
+        } else if (val == 10) {
+            count--;
+        }
+        printf("Current count: %i\n", count);
     }
     return 0;
 }
