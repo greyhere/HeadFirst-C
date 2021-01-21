0287df1b07911b777e115690b1ad480cbe0c327d
greyhere <57859708+greyhere@users.noreply.github.com>
2021-01-21 12:51:22 +0000

    add text search program

---
+++ text_search.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+
+char tracks[][80] = {
+    "I left my heart in Havard Med School",
+    "Newark, Newark - a wonderful town",
+    "Dancing with a Dork",
+    "From here to maternity",
+    "The girl from Iwo Jima",
+};
+
+
+void find_track(char query[])
+{
+    int i;
+    for (i = 0; i < 5; i++) {
+        if (strstr(tracks[i], query))
+            printf("Track %i: '%s'\n", i, tracks[i]);
+    }
+}
+
+
+int main()
+{
+    char search_for[80];
+    printf("Search for: ");
+    fgets(search_for, 80, stdin);
+    find_track(search_for);
+    return 0;
+}
